853598390a0b42faa1904c660cbf826c817b420d
Harris Marfel <hrace009@gmail.com>
2021-12-01 20:16:54 +0700

    Add Indonesian Language

---
+++ src/lang/id/pw-api-messages.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Harris Marfel <hrace009@gmail.com>
+ * @link https://www.hrace009.com
+ * @copyright Copyright (c) 2020.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | PW API Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are in this package.
+    |
+    */
+
+    'server_connect_failed' => 'Server sedang offline, system tidak bisa memproses permintaan anda',
+    'no_version' => 'Saat ini tidak ada file protokol untuk versi (:version).'
+
+];
